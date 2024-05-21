d500baa782ca85b83281100df8acda3a46a1652d
Anshad UK <anshadu@gmail.com>
2024-05-21 23:13:05 +0530

    Move cart item to Order_Product and Reduce Quantity of sold

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,6 @@ from orders import views
 urlpatterns = [
     path('place_order/',views.place_order,name='place_order'),
     path('payments/',views.payments,name='payments'),
-
+    path("order_complete/", views.order_complete, name="order_complete"),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,14 +2,60 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm 
-from .models import Order
+from .models import Order,Payment,OrderProduct
 import datetime
 import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from store.models import Product 
 
 # Create your views here.
+@csrf_exempt
 def payments(request):
     body = json.loads(request.body)
+    print("hellloo")
     print(body)
+    order = Order.objects.get(
+        user = request.user,is_ordered=False,order_number=body['orderID']
+    )
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status' ],
+    )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+    print(order)
+
+    # move the cart item to the order product table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct() 
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True  
+        orderproduct.save()
+        print(orderproduct)
+
+
+        #Reduce the quantity of the sold products
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+
+    #Clear cart
+    CartItem.objects.filter(user=request.user).delete()
+    
+ 
     return render(request,'orders/payments.html')
 
 def place_order(request,total=0,quantity=0):
@@ -71,3 +117,6 @@ def place_order(request,total=0,quantity=0):
             return render(request,'orders/payments.html',context)
     else:
         return redirect('checkout')
+    
+def order_complete(request):
+    return HttpResponse(order_complete)      
